43e4a3b3e461d3d22127e98e5d98932519ee4203
boxin <begin161109@163.com>
2017-09-03 19:43:19 +0800

    user model commit

---
+++ src/main/java/com/mmall/common/Const.java
@@ -0,0 +1,14 @@
+package com.mmall.common;
+
+/**
+ * Created by wang on 2017/9/3.
+ */
+public class Const {
+    public static final String CURRENT_USER="current_user";
+    public static final String EMAIL = "email";
+    public static final String USERNAME = "username";
+    public interface Role{
+        int ROLE_CUSTOMER = 0;
+        int ROLE_ADMIN = 1;
+    }
+}
